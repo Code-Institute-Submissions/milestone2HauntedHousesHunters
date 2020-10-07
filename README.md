2d2159bb391487ab435b0488ea5ad24e42b257f2
laumartin <lauratemar@gmail.com>
2020-10-07 22:48:33 +0000

    Modifying form on contact.html so the selection field contains the actual names of the houses. Modifying style as some parts were not consistent with the overallsite, adding new font for headings on all pages. Removing unused code and unused files. Updating main.js with the information for the infowindow section of the map with all the houses data, and applying more style to the map markers. Modifying structure of locations.html with better design, avoiding overlapping among divs

--- README.md
+++ README.md
@@ -1,20 +1,200 @@
-<img src=".png" style="margin: 0;">
+<img src="images/logo.png" style="margin: 0; height:70px; width:200px">
 
 
-The Second milestone project from the Full Stack Developer course with Code Institute.
 ## Description
+The Second milestone project from the Full Stack Developer course with Code Institute.
+One or two paragraphs providing an overview of your project. Essentially, this part is your sales pitch.
 
 The project is made based on the second module of the course that covers HTML, CSS, JavaScript. It also uses Jquery, Google Maps API and EmailJS
 
-## Updates 
+----
+
+## UX/UI 
+List your user stories, explain what they are looking for on your website.
+
+* Fonts Used        
+The site only uses 2 font families. The idea was to use something according to 
+the site topic, within vintage/horror lines. So the fonts chosen were:         
+    1. monospace (as it looks like text written from old typewritter, it provides clarity to the 
+    text and nice natural letter spacing easy to read).  Used for all texts    
+    2. Jessen Cicero (external webfont as none of the usual ones matched the criteria) it gives a look 
+    half way among vintage and horror. Used on text headings, navigation menu and buttons.
+
+* Color palette used        
+Only 4 plane colours have been used in the site for consistency. The only variation has been in opacity for some cases.     
+#8d0134 red     
+#c0b43c gold        
+#fbf8f2 whitish     
+#231f20 blackish
+
+* Layouts explain why you laid things out the way you did. 
+
+* Wireframes mockups.       
+As part of the design process I've used Balsamiq wireframes tool to create and visualize an 
+initial structure of what the site was going to be. 
+These files are included as a pdf file in the project itself (in an separate directory wireframes folder). 
+There has been several changes during the project development from what the initial wireframes were:        
+    1. Hero image from houses.html was deleted as it created worse UI since this page contains the cards info 
+    of all houses, which should be the main focus of the user and not get distracted with the big hero image.       
+    2. The locations page, some text and image has been added later.
+
+
+----
+
+## Features
+These are the different parts of the project, and describe each in a sentence or so.
+
+<dl>
+<dt>Existing Features</dt>
+
+* Feature 1 - allows users X to achieve Y, by having them fill out Z
+For some/all of your features, you may choose to reference the specific project files that implement them, although this is entirely optional.
+
+      
+
+<dt>Future development</dt>
+
+* Styling the Google Map change default colors design so it better matches the vintage style or colors from the site.
+
+* Adding more UI side effects with JQuery, like in the Slideshow that is currently made only with HTML and CSS with a very fixed paramethers creating some limitations
+regarding positioning, styling with borders and resizing it in a more responsible and easier way.
+
+
+</dl>
+
+----
 
 ## Testing
 This site was tested across multiple device screen sizes to ensure compatibility and responsiveness.
-We are using Jasmine to run some testing. The specs folder is where I've put the tests.
-The source code that we're going to be running the tests against is in the scripts folder,
-which contains a copy of our js folder.
+
+ go over all of your user stories from the UX section and ensure that they all work as intended, 
+with the project providing an easy and straightforward way for the users to achieve their goals.
+
+Validator for html code     
+[https://validator.w3.org/]
+
+Validator for css code      
+[https://jigsaw.w3.org/css-validator/]
+
+A particularly useful form for describing your testing process is via scenarios, such as:
+
+Contact form:
+Go to the "Contact Us" page
+Try to submit the empty form and verify that an error message about the required fields appears
+Try to submit the form with an invalid email address and verify that a relevant error message appears
+Try to submit the form with all inputs valid and verify that a success message appears.
+In addition, you should mention in this section how your project looks and works on different browsers and screen sizes.
+
+Known problems      
+discovered during your testing, even if you haven't addressed them yet.
+
+----
+## Deployment
+steps to do that in the github settings. 
+Basically explain, how you got the page up and running.
+https://help.github.com/en/articles/configuring-a-publishing-source-for-github-pages 
+deployment includes writing up how to run your code locally. So explain how to pull your code from your github repository 
+so someone could clone your code to work on on their own machine. 
+Here is how to do that: https://help.github.com/en/articles/cloning-a-repository Include these steps in your readme.md too 
+and that's the deployment section of your readme sorted
+
+----
+## Technologies used
+[HTML](https://html.spec.whatwg.org/multipage/) Basic structure for the site        
+[CSS](https://www.w3.org/Style/CSS/Overview.en.html) Styling the website    
+[Bootstrap](https://getbootstrap.com/) customize responsive mobile-first mainly used for grid structure and buttons      
+[JavaScript](https://www.w3schools.com/js/) Site interaction. Google Maps      
+[JQuery](https://jquery.com/) To simplify DOM manipulation. Readmore toggler      
+[Google Maps API](https://cloud.google.com/maps-platform) Provide locations of the houses on world map for better user experience        
+[EmailJS API](https://www.emailjs.com/) Setting email service to receive form messages sent from the contact.html page    
+[Balsamiq wireframes](https://balsamiq.com/) Initial mockups UI design ideas for the site
 
 
---------
+----
+## Resources used 
+<dl>
+<dt>Images:</dt>
+
+* castle.jpg image.  
+Licence. Cornell University's Institute for Digital Collections  
+[Source Link](http://fantastic.library.cornell.edu/imagerecord.php?record=134)      
+Used on Locations.html page
+
+
+* castleinterior.png image.  
+Licence. Cornell University's Institute for Digital Collections  
+[Source Link](http://fantastic.library.cornell.edu/imagerecord.php?record=134)      
+Used as background on index.html page
+
+
+<dt>Fonts:</dt>
+
+* Jessen Font Family.  
+Licence. licensed under the SIL Open Font License. Copyright 2019 Gotico-Antiqua project authors (thomas.huot-marchand@anrt-type.fr)  
+[Source Link](https://www.1001fonts.com/jessen-font.html)       
+Used on navigation menu, buttons and text headings
+
+<dt>Icons:</dt>
+
+* gate.svg     
+Licence. Icons on UXWing free to use any personal, commercial projects.         
+[Source Link](https://uxwing.com/gate/)     
+Used on google map as marker
+
+* contact-icon.png      
+Licence Shutterstock standard licence. Royalty-free stock vector created by AVA Bitter id 591497954     
+[Source Link](https://www.shutterstock.com/image-vector/handdrawn-vintage-typewriter-writing-machine-publishing-591497954)      
+Used in formulary on contact.html
+
+<dt>Texts sources:</dt>
+
+House1 Garden Reach. Location: Cheltenham, UK
+[https://labitacoradelmiedo.wordpress.com/category/lugares-encantados/]
+[https://www.spookyisles.com/was-the-chelthenham-poltergeist-the-real-woman-in-black/]
+[https://en.wikipedia.org/wiki/]
+
+House2 Boleskine House. Location: Foyers Scotland, UK
+[https://www.abc.es/viajar/top/20141030/abci-casas-encantadas-fantasma-201410031336_8.html]     
+[https://en.wikipedia.org/wiki/]
+
+House3 Palazzo Dario. Venezia, Italy        
+[https://www.abc.es/viajar/top/20141030/abci-casas-encantadas-fantasma-201410031336_8.html] page8       
+[https://en.wikipedia.org/wiki/]
+
+House4 Palacio de Linares. Madrid, Spain
+[https://www.abc.es/viajar/top/20141030/abci-casas-encantadas-fantasma-201410031336_8.html] page10
+[https://en.wikipedia.org/wiki/]
+
+House5 Catacombs of Paris. Location: Paris, France      
+[https://en.wikipedia.org/wiki/]
+
+House6 Leap Castle. Location : Coolderry, Ireland       
+[https://en.wikipedia.org/wiki/]
+
+House7 Amityville House. Location: New York, USA
+[https://www.abc.es/viajar/top/20141030/abci-casas-encantadas-fantasma-201410031336_8.html] page2       
+[https://en.wikipedia.org/wiki/]
+
+House8 LaLaurie Mansion. Location: New Orleans, USA
+[https://www.abc.es/viajar/top/20141030/abci-casas-encantadas-fantasma-201410031336_8.html] page5
+[https://es.wikipedia.org/wiki/Delphine_LaLaurie#Mansi%C3%B3n_LaLaurie]
+
+House9 Isla de las Muñecas. Location : Mexico city
+[https://es.wikipedia.org/wiki/Isla_de_las_mu%C3%B1ecas]
+
+House10 Joelma Building. Location: Sao Paulo, Brazil
+[https://traveltriangle.com/blog/haunted-places-in-brazil/]
+
+House11 El Salto Hotel. Location: Tequendama, Colombia
+[https://culturacolectiva.com/photography/the-most-haunted-spots-in-latin-america]
+
+House 12 Keg Mansion, Toronto       
+[https://www.readersdigest.ca/travel/canada/canadas-10-most-haunted-places/]
+
+</dl>
+
+
+
+
 
 
--- js/main.js
+++ js/main.js
@@ -48,17 +48,6 @@
          $('#complete12').toggle();
    });
 
-   /*
-$(document).ready(function() {
-   $(".house3_read").on("click", function() {
-        $(".house1").removeClass('highlight_house');
-        $(".house2").removeClass('highlight_house');
-        $(".house3").removeClass('highlight_house');
-        $(".house3").addClass('highlight_house');
-   });
-}); 
-
-*/
 
 /*------------Contact page EmailJS-------------------- */
 function sendMail(contactForm){
@@ -80,64 +69,16 @@ function sendMail(contactForm){
 
 /*-------------------------Locations page GoogleMaps API --------------------------- */
 
-/*-----------Option 1 only one marker with info. (working)-----------
-function initMap(){
-    const house1 = {lat:51.901716 , lng: -2.077738};
-    const map = new google.maps.Map(document.getElementById("map"),{
-        zoom:4,
-        center:house1,
-        styles:'map-retro'
-    });
-
-    const contentString =
-        '<div id="content">' +
-            '<h4>House 1. Garden Reach</h4>' +
-            '<div id="bodyContent">' +
-                "<p><b>Location: Cheltenham, UK </b></p>" +
-                '<div><img border="1" align="center" height="100px" width="100px" SRC="images/house1.png"></div>'+
-                "<p>The building was the home of the Despard family whose daughter Rosina saw the ghost of a veiled" +
-                "woman on several occasions in the 1880s</p>" +
-                '<p>Attribution: Garden Reach, <a href="https://en.wikipedia.org/wiki/The_Cheltenham_Ghost">' +
-                "Wikipedia</a> </p>" +
-            "</div>" +
-        "</div>";
-        
-    const infowindow = new google.maps.InfoWindow({
-        content: contentString,
-        maxWidth: 300,
-    });
-
-    const marker = new google.maps.Marker({
-        position: house1,
-        icon: {
-            path: "M26.21,28.03h7.77v-9.36c0-1.4,1.14-2.54,2.54-2.54s2.54,1.14,2.54,2.54v7.65c2.5-1.17,5.09-2.12,7.76-2.85v-9.56 c0-1.41,1.14-2.54,2.54-2.54c1.41,0,2.54,1.14,2.54,2.54v8.47c2.23-0.35,4.52-0.55,6.88-0.59l0.08,0V8.86 c0-1.4,1.14-2.54,2.54-2.54c1.4,0,2.54,1.14,2.54,2.54v13.09c2.26,0.18,4.58,0.5,6.96,0.97v-9.01c0-1.41,1.14-2.54,2.54-2.54 c1.41,0,2.54,1.14,2.54,2.54v10.23c2.52,0.7,5.11,1.57,7.76,2.6v-8.07c0-1.4,1.14-2.54,2.54-2.54c1.4,0,2.54,1.14,2.54,2.54v9.36 h7.82v-6.91h-2.31c-1.34,0-2.43-1.09-2.43-2.43v-1.21c0-1.34,1.09-2.43,2.43-2.43h3.67c-0.69-1.39-1.07-2.96-1.07-4.61 C96.95,4.67,101.63,0,107.39,0c5.77,0,10.44,4.67,10.44,10.44c0,1.66-0.39,3.22-1.07,4.61h3.7c1.34,0,2.43,1.09,2.43,2.43v1.21 c0,1.34-1.09,2.43-2.43,2.43h-2.31v86.53H96.67v-7.44H86.48c-0.06,0-0.11,0.01-0.17,0.01c-0.06,0-0.12,0-0.17-0.01H73.72 c-0.08,0.01-0.17,0.01-0.25,0.01c-0.08,0-0.17,0-0.25-0.01H61.65c-0.08,0.01-0.16,0.01-0.24,0.01c-0.08,0-0.16,0-0.24-0.01H49.62 c-0.08,0.01-0.17,0.01-0.25,0.01c-0.08,0-0.17,0-0.25-0.01H36.69c-0.06,0-0.11,0.01-0.17,0.01c-0.06,0-0.12,0-0.17-0.01H26.21v7.44 H4.74V21.12H2.43C1.09,21.12,0,20.03,0,18.69v-1.21c0-1.34,1.09-2.43,2.43-2.43H6.1c-0.69-1.39-1.07-2.96-1.07-4.61 C5.02,4.67,9.7,0,15.46,0S25.9,4.67,25.9,10.44c0,1.66-0.39,3.22-1.07,4.61h3.7c1.34,0,2.43,1.09,2.43,2.43v1.21 c0,1.34-1.09,2.43-2.43,2.43h-2.31V28.03L26.21,28.03z M51.91,52.07h6.96V37.65c-2.39,0.04-4.71,0.27-6.96,0.66V52.07L51.91,52.07z M63.96,52.07h6.96V38.9c-2.39-0.52-4.71-0.87-6.96-1.07V52.07L63.96,52.07z M70.92,68.19h-6.96v26.95h6.96V68.19L70.92,68.19z M58.87,68.19h-6.96v26.95h6.96V68.19L58.87,68.19z M60.21,60.82l-1.59,4.15h5.58l-1.47-4.21c0.93-0.47,1.58-1.45,1.58-2.57 c0-1.59-1.3-2.89-2.89-2.89c-1.59,0-2.89,1.3-2.89,2.89C58.53,59.35,59.21,60.36,60.21,60.82L60.21,60.82L60.21,60.82z M96.67,33.1 h-7.82v5.75h7.82V33.1L96.67,33.1z M96.67,43.91h-7.82v51.23h7.82V43.91L96.67,43.91z M26.21,95.14h7.77V43.91h-7.77V95.14 L26.21,95.14z M26.21,38.85h7.77V33.1h-7.77V38.85L26.21,38.85z M76.01,29.4v5.56c2.52,0.7,5.11,1.57,7.76,2.6v-5.37 C81.11,31.08,78.52,30.15,76.01,29.4L76.01,29.4z M76.01,40.22v54.93h7.76V43.01C81.11,41.9,78.52,40.97,76.01,40.22L76.01,40.22z M70.92,33.74v-5.66c-2.39-0.52-4.71-0.87-6.96-1.07v5.75C66.22,32.95,68.54,33.27,70.92,33.74L70.92,33.74z M58.87,26.83 c-2.39,0.04-4.71,0.27-6.96,0.66v5.7c2.23-0.35,4.52-0.55,6.88-0.59l0.08,0V26.83L58.87,26.83z M46.82,28.72 c-2.69,0.83-5.27,1.91-7.76,3.23v5.19c2.5-1.17,5.09-2.12,7.76-2.85V28.72L46.82,28.72z M46.82,39.54 c-2.69,0.83-5.27,1.91-7.76,3.23v52.37h7.76V39.54L46.82,39.54z",
-            scale: 0.30,
-            strokeWeight: 1,
-            fillColor:'red',
-            strokeColor:'red'
-        },
-        map,
-        title: "Garden Reach House. Location: Cheltenham, UK",
-    });
-
-    marker.addListener("click", () => {
-        infowindow.open(map, marker);
-    });
-
-----------------------------------*/
-
-
-   /* -----------Option 4 several markers but no info ---------*/
    function initMap(){
         var map = new google.maps.Map(document.getElementById("map"),{
             zoom:3,
             center:{
-                lat:40.419971 , 
-                lng: -3.692151
+                lat:31.704726 , 
+                lng: -39.471655
             }
         }); 
    
-   var labels = "ABCDEFGHIJKL";
+   var labels = "123456789JKL";
 
     var locations = [
         {location: {lat:51.903023 , lng: -2.061397}, message: 
@@ -177,15 +118,114 @@ function initMap(){
                     "Wikipedia</a> </p>" +
                 "</div>" +
             "</div>"},
-        {location: {lat:40.419971, lng:-3.692151}, message: 'Scarey house 4'}, /*House 4 Palacio de Linares, madrid*/
-        {location: {lat:48.833988 , lng:2.332444}, message: 'Scarey house 5'}, /*House 5 catacombs of paris*/
-        {location: {lat:53.028119 , lng:-7.808400}, message: 'Scarey house 6'}, /*House 6  Leap Castle. Coolderry*/
-        {location: {lat:40.6789893 , lng:-73.4170673}, message: 'Scarey house 7'}, /*House 7  Amityville House */
-        {location: {lat:29.962072 , lng:-90.061264}, message: 'Scarey house 8'}, /*House 8  LaLaurie Mansion */
-        {location: {lat:19.290305 , lng:-99.096538}, message: 'Scarey house 9'}, /*House 9 isla de las muñecas mexico*/
-        {location: {lat:-23.547790 , lng:-46.641106}, message: 'Scarey house 10'},/*House 10 Joelma Building. Sao Paulo*/
-        {location: {lat:4.576359 , lng:-74.296661}, message: 'Scarey house 11'}, /*House 11 El Salto Hotel. Tequendama*/
-        {location: {lat:43.667025 , lng:-79.377915}, message: 'Scarey house 12'} /*House 12 Keg Mansion. Toronto*/
+        {location: {lat:40.419971, lng:-3.692151}, message: 
+        '<div id="content">' +
+            '<h5>House 4. Palacio de Linares. </h5>' +
+                '<div id="bodyContent">' +
+                    "<p><b>Location: Madrid, Spain </b></p>" +
+                    '<div><img border="1" height="100px" width="100px" style="float:right" SRC="images/house4.jpg"></div>'+
+                    '<p>Psychophonies, mysterious images and dozens of stories talk about a child that runs around' + 
+                    'the corridors, stairs and salons looking for her mother</p>' +
+                    '<p>Attribution: ..... <a href="https://es.wikipedia.org/wiki/Palacio_de_Linares/">' +
+                    "Wikipedia</a> </p>" +
+                "</div>" +
+            "</div>"}, 
+        {location: {lat:48.833988 , lng:2.332444}, message: 
+        '<div id="content">' +
+            '<h5>House 5. Catacombs of Paris.</h5>' +
+                '<div id="bodyContent">' +
+                    "<p><b>Location: Paris, France </b></p>" +
+                    '<div><img border="1" height="100px" width="100px" style="float:right" SRC="images/house5.jpg"></div>'+
+                    '<p>The underground ossuaries, which hold the remains of more than six million people in a small part of a tunnel' + 
+                    'network built to consolidate Paris ancient stone quarries.</p>' +
+                    '<p>Attribution: ..... <a href="https://en.wikipedia.org/wiki/Catacombs_of_Paris">' +
+                    "Wikipedia</a> </p>" +
+                "</div>" +
+            "</div>"}, 
+        {location: {lat:53.028119 , lng:-7.808400}, message: 
+        '<div id="content">' +
+            '<h5>House 6. Leap Castle. </h5>' +
+                '<div id="bodyContent">' +
+                    "<p><b>Location: Coolderry, Ireland </b></p>" +
+                    '<div><img border="1" height="100px" width="100px" style="float:right" SRC="images/house6.jpg"></div>'+
+                    '<p>Constructed most likely around 1250 CE. by the O Bannon clan that were the "secondary chieftains" of the territory' + 
+                    'and were subject to the ruling O Carroll clan </p>' +
+                    '<p>Attribution: ..... <a href="https://en.wikipedia.org/wiki/Leap_Castle">' +
+                    "Wikipedia</a> </p>" +
+                "</div>" +
+            "</div>"}, 
+        {location: {lat:40.6789893 , lng:-73.4170673}, message: 
+        '<div id="content">' +
+            '<h5>House 7. Amityville House. </h5>' +
+                '<div id="bodyContent">' +
+                    "<p><b>Location: New York, USA </b></p>" +
+                    '<div><img border="1" height="100px" width="100px" style="float:right" SRC="images/house7.jpg"></div>'+
+                    '<p>Became famous in 1974 when the DeFeo family was murdered by the eldest son with a rifle, while his parents' + 
+                    'and four siblings were sleeping.</p>' +
+                    '<p>Attribution: ..... <a href="https://en.wikipedia.org/wiki/Amityville,_New_York#The_Amityville_Horror">' +
+                    "Wikipedia</a> </p>" +
+                "</div>" +
+            "</div>"},
+        {location: {lat:29.962072 , lng:-90.061264}, message: 
+        '<div id="content">' +
+            '<h5>House 8. LaLaurie Mansion. </h5>' +
+                '<div id="bodyContent">' +
+                    "<p><b>Location: New Orleans, USA </b></p>" +
+                    '<div><img border="1" height="100px" width="100px" style="float:right" SRC="images/house8.jpg"></div>'+
+                    '<p>The story of this building goes back to the XVIII century. His owner Marie Delphine Lalaurie apparently' +
+                    'had a double life, public facing she showed herself as very kind with people of black race and worrying about her slaves health.</p>' +
+                    '<p>Attribution: ..... <a href="https://en.wikipedia.org/wiki/Delphine_LaLaurie">' +
+                    "Wikipedia</a> </p>" +
+                "</div>" +
+            "</div>"}, 
+        {location: {lat:19.290305 , lng:-99.096538}, message: 
+        '<div id="content">' +
+            '<h5>House 9. Isla de las Muñecas. </h5>' +
+                '<div id="bodyContent">' +
+                    "<p><b>Location: Xochimilco, Mexico</b></p>" +
+                    '<div><img border="1" height="100px" width="100px" style="float:right" SRC="images/house9.jpg"></div>'+
+                    '<p>Originally owned by Don Julian Santana Barrera, is full of dolls hanging from trees and buildings covered with cobwebs and insects' +
+                     'The place was named during the 1950s when the owner began to hang them as protection against evil spirits. </p>' +
+                    '<p>Attribution: Wikipedia <a href="https://en.wikipedia.org/wiki/Xochimilco">' +
+                    "Wikipedia</a> </p>" +
+                "</div>" +
+            "</div>"}, 
+        {location: {lat:-23.547790 , lng:-46.641106}, message: 
+        '<div id="content">' +
+            '<h5>House 10. Joelma Building</h5>' +
+                '<div id="bodyContent">' +
+                    "<p><b>Location: Sao Paulo, Brazil </b></p>" +
+                    '<div><img border="1" height="100px" width="100px" style="float:right" SRC="images/house10.jpg"></div>'+
+                    '<p>One of the top haunted places to visit in Brazil.  Built in 1971, on 1 February 1974, an air conditioning' +
+                     'unit overheated, starting a fire. There were 756 people in the building at the time.</p>' +
+                    '<p>Attribution: Wikipedia <a href="https://en.wikipedia.org/wiki/Joelma_Building">' +
+                    "Wikipedia</a> </p>" +
+                "</div>" +
+            "</div>"},
+        {location: {lat:4.576359 , lng:-74.296661}, message: 
+        '<div id="content">' +
+            '<h5>House 11. El Salto Hotel</h5>' +
+                '<div id="bodyContent">' +
+                    "<p><b>Location: Tequendama, Colombia</b></p>" +
+                    '<div><img border="1" height="100px" width="100px" style="float:right" SRC="images/house11.png"></div>'+
+                    '<p>El Salto was a functional hotel from 1924 to 1990 located near the waterfalls of Tequendama. Since it is' +
+                     'at the top of a hill, legend says that many people would book a night at the hotel and then throw themselves off the cliff.</p>' +
+                    '<p>Attribution: Deserted Places <a href="http://desertedplaces.blogspot.com/2012/08/the-haunted-hotel-at-tequendama-falls.html">' +
+                    "Wikipedia</a> </p>" +
+                "</div>" +
+            "</div>"},
+        {location: {lat:43.667025 , lng:-79.377915}, message: 
+        '<div id="content">' +
+            '<h5>House 12. Keg Mansion</h5>' +
+                '<div id="bodyContent">' +
+                    "<p><b>Location: Toronto, Canada</b></p>" +
+                    '<div><img border="1" height="100px" width="100px" style="float:right" SRC="images/house12.jpg"></div>'+
+                    '<p>Originally built in 1867. It was once the home of one of the most prominent families in the city, The Masseys. As legend has it,'+
+                     'in 1915, after the death of Massey’s beloved only daughter, Lillian, one of the maids was so stricken by grief that she hung herself.</p>' +
+                    '<p>Attribution: Wikipedia <a href="https://en.wikipedia.org/wiki/Keg_Mansion">' +
+                    "Wikipedia</a> </p>" +
+                "</div>" +
+            "</div>"} 
     ];
     
     var markers = locations.map(function(location, i){
@@ -200,11 +240,9 @@ function initMap(){
         console.log(location);
         var mark = new google.maps.Marker({
             position: location.location,
-            label: labels[i % labels.length],
+            label:"",
             icon: {
                     strokeWeight: 1,
-                    strokeColor:'red',
-                    fillColor:'red',
                     url:"images/gate.svg",
                     scaledSize: new google.maps.Size(20, 20), // scaled size
                     origin: new google.maps.Point(0,0), // origin
@@ -227,6 +265,6 @@ function initMap(){
         imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
     });
 }
-    /*--------------------------------------------*/
+    
 
     
